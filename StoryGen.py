7138798b067f02757f969b84549aee78757c82a1
Spherox <65167045+NathanielJames97@users.noreply.github.com>
2024-04-14 18:18:32 +0100

    Initial tkinter setup & .gitignore

--- StoryGen.py
+++ StoryGen.py
@@ -1,1 +1,19 @@
 
+import numpy as np
+import tkinter as tk
+
+#tkinter setup screen
+Screen = Tk()
+Screen.title("Nathaniel James 'Madlibs' project")
+Screen.geometry('1000x1000')
+Screen.config(bg="#F5B847")
+Label(Screen, text='Nathaniel James Madlibs project').place(x=100, y=20)
+#creating buttons
+Story1Button = Button(Screen, text='A memorable day', font=("Lato", 13),command=lambda: Story1(Screen),bg='White')
+Story1Button.place(x=140, y=90)
+Story2Button = Button(Screen, text='Ambitions', font=("Lato", 13),command=lambda: Story2(Screen), bg='White')
+Story2Button.place(x=150, y=150)
+ 
+Screen.update()
+Screen.mainloop()
+
